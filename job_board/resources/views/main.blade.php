a75df8b357ed67e342067f25b7cd056a391b7d49
AleksandraHlukhova <a0993715642@gmail.com>
2020-07-25 12:04:41 +0300

    add main.blade

---
+++ job_board/resources/views/main.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="blog-header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-8">
+                    <a class="blog-header-logo text-dark" href="#">Job Board</a>
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-center">
+                    <a class="btn btn-sm btn-outline-secondary" href="#">Add vacation</a>
+                </div>
+            </div>
+        </header>
+
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 text-muted" href="#">JavaScript</a>
+                <a class="p-2 text-muted" href="#">Java</a>
+                <a class="p-2 text-muted" href="#">C#</a>
+                <a class="p-2 text-muted" href="#">Pyton</a>
+                <a class="p-2 text-muted" href="#">PHP</a>
+                <a class="p-2 text-muted" href="#">C++</a>
+                <a class="p-2 text-muted" href="#">Ruby</a>
+            </nav>
+            <hr>
+            <div class="block-info">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nulla labore eligendi molestiae debitis natus qui beatae aperiam blanditiis cupiditate ex dignissimos, animi ab error voluptatem magnam impedit vitae sunt deleniti fugit placeat! Iste esse inventore placeat illum optio blanditiis delectus distinctio ut, temporibus reprehenderit beatae molestiae? Eius, incidunt vel maxime harum omnis neque unde molestiae fugiat, culpa dolores voluptatibus eum consectetur esse quo! Quidem consequatur sequi nam recusandae illo architecto ullam exercitationem praesentium quam provident perferendis obcaecati, repellendus vel debitis inventore quaerat animi possimus earum ipsam voluptates! Iusto expedita ipsam unde laborum culpa, quia nemo voluptatem dolores magnam maxime? Pariatur id illum, quos recusandae culpa quis nam autem veritatis at facere dolorem nostrum sequi nemo enim. Fuga nesciunt odit provident et veritatis perspiciatis magni quam amet eaque quaerat, ducimus esse consequuntur enim at omnis officia maxime architecto expedita deserunt aperiam consequatur animi optio? Voluptates doloribus error sunt veniam earum itaque dignissimos, nihil, iure adipisci necessitatibus quisquam. Repellendus iste, dignissimos consequatur blanditiis nemo eligendi eum quos debitis aut unde nam ipsam autem sunt repudiandae id adipisci. Earum dicta cumque sapiente ab, reprehenderit eveniet dolor temporibus consequatur in quam dolorum iusto amet atque rerum perspiciatis quos? Amet, qui iure? Accusantium, rem.
+            </div>
+            <hr>
+            <footer class="blog-footer flex-content-center">
+                <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a
+                        href="https://twitter.com/mdo">@mdo</a>.</p>
+                <p>
+                    <a href="#">Back to top</a>
+                </p>
+            </footer>
+        </div>
+
+</body>
+
+</html>
